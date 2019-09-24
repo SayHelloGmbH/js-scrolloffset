a36e2e4cb9957e554df3198a0c81ed4a977209a7
Mark Howells-Mead <permanent.tourist@gmail.com>
2019-09-24 12:01:44 +0200

    --js-scrolloffset var and package maintenance

--- index.js
+++ index.js
@@ -2,18 +2,23 @@
  * Watch window scroll position and add/remove
  * CSS class to the HTML object as appropriate
  *
- * Modify sayhellogmbh_js_scrolloffset in your own code if you want to react
+ * Modify CSS variable --js-scrolloffset in your own code if you want to react
  * after a certain number of pixels - e.g. 50 will add/remove the class when
  * the window scroll offset is 50px.
+ *
+ * Beware: if the window scroll position is changed by a change in CSS when
+ * this value is set, a flickering will occur.
  */
 
-var sayhellogmbh_js_scrolloffset = 0;
+document.documentElement.style.setProperty('--js-scrolloffset', 0);
 
 (function($) {
     // Track window scroll position and apply CSS class to HTML element
     var hasScrollOffset = function() {
-        if ($(window).scrollTop() > sayhellogmbh_js_scrolloffset) {
-            $('html').addClass('s-has--scrolloffset');
+        if ($(window).scrollTop() > parseInt(document.documentElement.style.getPropertyValue('--js-scrolloffset'))) {
+            if (!$('html').hasClass('s-has--scrolloffset')) {
+                $('html').addClass('s-has--scrolloffset');
+            }
         } else {
             $('html').removeClass('s-has--scrolloffset');
         }
