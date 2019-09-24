f71118808e7b01248b4c0754ed0d287e3d7e74a2
Mark Howells-Mead <permanent.tourist@gmail.com>
2019-09-24 11:01:06 +0200

    Add sayhellogmbh_js_scrolloffset variable

--- README.md
+++ README.md
@@ -7,11 +7,19 @@ class to the HTML object as appropriate.
 
 By including this script, no further action is necessary. The CSS class name
 `s-has--scrolloffset` will be applied and can be referenced in CSS:
-`html.s-has--scrolloffset {…}`. The class name convention adheres to BEM 
+`html.s-has--scrolloffset {…}`. The class name convention adheres to BEM
 naming conventions: `s-has` being the block identifier and `--scrolloffset`
 being the modifier.
 
+Modify sayhellogmbh_js_scrolloffset in your own code if you want to react
+after a certain number of pixels - e.g. 50 will add/remove the class when
+the window scroll offset is 50px.
+
 ## Changelog
+
+### 1.1.0 2019-09-24
+
+* Add sayhellogmbh_js_scrolloffset control variable.
 
 ### 1.0.0 2019-07-05
 
--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 /**
  * Watch window scroll position and add/remove
  * CSS class to the HTML object as appropriate
+ *
+ * Modify sayhellogmbh_js_scrolloffset in your own code if you want to react
+ * after a certain number of pixels - e.g. 50 will add/remove the class when
+ * the window scroll offset is 50px.
  */
 
-(function($) {
-	// Track window scroll position and apply CSS class to HTML element
-	var hasScrollOffset = function() {
-		if ($(window).scrollTop() > 0) {
-			$('html').addClass('s-has--scrolloffset');
-		} else {
-			$('html').removeClass('s-has--scrolloffset');
-		}
-	};
+var sayhellogmbh_js_scrolloffset = 0;
 
-	$(document).on('ready.scrollpos', hasScrollOffset);
-	$(window).on('load.scrollpos scroll.scrollpos', hasScrollOffset);
+(function($) {
+    // Track window scroll position and apply CSS class to HTML element
+    var hasScrollOffset = function() {
+        if ($(window).scrollTop() > sayhellogmbh_js_scrolloffset) {
+            $('html').addClass('s-has--scrolloffset');
+        } else {
+            $('html').removeClass('s-has--scrolloffset');
+        }
+    };
+
+    $(document).on('ready.scrollpos', hasScrollOffset);
+    $(window).on('load.scrollpos scroll.scrollpos', hasScrollOffset);
 
 })(jQuery);
